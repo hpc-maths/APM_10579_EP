b4a0153f760af9ea7e25b6bda9d8a4bfe513686f
Johan Mabille <johan.mabille@gmail.com>
2022-03-09 02:16:09 +0100

    missing slides and implementation of autodiff

--- courses/templates/autodiff/autodiff.hpp
+++ courses/templates/autodiff/autodiff.hpp
@@ -1,8 +1,15 @@
 #ifndef AUTODIFF_HPP
 #define AUTODIFF_HPP
 
+#include <tuple>
+#include <type_traits>
+
 namespace ad
 {
+
+
+
+
     template <class T>
     class variable
     {
@@ -31,7 +38,7 @@ namespace ad
         }
 
         template <class E>
-        variable& operator+=(const E& e)
+        variable& operator*=(const E& e)
         {
             *this = e * *this;
             return *this;
@@ -43,11 +50,26 @@ namespace ad
         T m_derivative;
     };
 
+    namespace detail
+    {
+        template <class T>
+        struct operand_type
+        {
+            using type = std::conditional_t<
+                    std::is_arithmetic<T>::value,
+                    variable<T>,
+                    const T&>;
+        };
+
+        template <class T>
+        using operand_type_t = typename operand_type<T>::type;
+    }
+
     template <class E1, class E2>
     struct binary_add
     {
-        using value_type = std::common_type_t<typename E1::value_type,
-                                              typename E2::value_type>;
+        using value_type = std::common_type_t<typename std::decay_t<E1>::value_type,
+                                              typename std::decay_t<E2>::value_type>;
         value_type operator()(const E1& e1, const E2& e2) const
         {
             return e1.value() + e2.value();
@@ -62,8 +84,8 @@ namespace ad
     template <class E1, class E2>
     struct binary_mul
     {
-        using value_type = std::common_type_t<typename E1::value_type,
-                                              typename E2::value_type>;
+        using value_type = std::common_type_t<typename std::decay_t<E1>::value_type,
+                                              typename std::decay_t<E2>::value_type>;
         value_type operator()(const E1& e1, const E2& e2) const
         {
             return e1.value() * e2.value();
@@ -75,50 +97,78 @@ namespace ad
         }
     };
 
-    template <class E1, class E2, class F>
-    class binary_op
+    template <class In, template <class...> class Out>
+    struct mpl_transform;
+
+    template <template <class...> class In, class... T, template <class...> class Out>
+    struct mpl_transform<In<T...>, Out>
+    {
+        using type = Out<T...>;
+    };
+
+    template <class In, template <class...> class Out>
+    using mpl_transform_t = typename mpl_transform<In, Out>::type;
+
+    template <template <class...> class F, class... T>
+    class universal_op
     {
     public:
 
-        using value_type = typename F::value_type;
+        using operand_type = std::tuple<detail::operand_type_t<T>...>;
 
-        binary_op(const E1& lhs, const E2& rhs)
-            : m_lhs(lhs)
-            , m_rhs(rhs)
+        //using functor_type = F<detail::operand_type_t<T>...>;
+        using functor_type = mpl_transform_t<operand_type, F>;
+
+        using value_type = typename functor_type::value_type;
+
+        template <class... U>
+        universal_op(U&&... u)
+            : m_operands(std::forward<U>(u)...)
             , m_functor()
         {
         }
 
         value_type value() const
         {
-            return m_functor(m_lhs, m_rhs);
+            return value_impl(std::make_integer_sequence<size_t, sizeof...(T)>());
         }
 
         value_type derivative() const
         {
-            return m_functor.derivative(m_lhs, m_rhs);
+            return derivative_impl(std::make_integer_sequence<size_t, sizeof...(T)>());
         }
 
     private:
 
-        const E1& m_lhs;
-        const E2& m_rhs;
-        F m_functor;
+        template <size_t... I>
+        value_type value_impl(std::integer_sequence<size_t, I...>) const
+        {
+            return m_functor(std::get<I>(m_operands)...);
+        }
+
+        template <size_t... I>
+        value_type derivative_impl(std::integer_sequence<size_t, I...>) const
+        {
+            return m_functor.derivative(std::get<I>(m_operands)...);
+        }
+        operand_type m_operands;
+        functor_type m_functor;
     };
 
 
 
     template <class E1, class E2>
-    binary_op<E1, E2, binary_add<E1, E2>> operator+(const E1& e1, const E2& e2)
+    universal_op<binary_add, E1, E2> operator+(const E1& e1, const E2& e2)
     {
-        return binary_op<E1, E2, binary_add<E1, E2>>(e1, e2);
+        return universal_op<binary_add, E1, E2>(e1, e2);
     }
 
     template <class E1, class E2>
-    binary_op<E1, E2, binary_mul<E1, E2>> operator*(const E1& e1, const E2& e2)
+    universal_op<binary_mul, E1, E2> operator*(const E1& e1, const E2& e2)
     {
-        return binary_op<E1, E2, binary_mul<E1, E2>>(e1, e2);
+        return universal_op<binary_mul, E1, E2>(e1, e2);
     }
+
     /***************************
      * variable implementation *
      ***************************/
@@ -217,4 +267,83 @@ template <class T>
 using add_reference_t = typename add_reference<T>::type;
 
 using reference = add_reference_t<T>;*/
+
+
+/*template <class... T>
+class my_class
+{
+    // ...
+};
+
+my_class<E1, E2> m1;
+my_class<E> m2;
+
+template <class T1, class T2>
+void func(T1 t1, T2 t2 = T2());
+
+template <class... T>
+void my_func(const T&... args)
+{
+    func(args...);
+}
+
+my_func(1., 42, "math");
+
+class... T => double, int, std::string
+const T&... => const double&, const int1, const std::string&
+
+template <class... T>
+void universal_func(T&&... t)
+{
+}
+
+template <class T>
+void universal_func(T&& t)
+{
+    call_func(std::forward<T>(t));
+}
+
+
+template <class T>
+void universal_func(std::vector<T>&& t); // Pas universal reference
+
+
+
+int x = 42;
+int& rx = x
+const int& cx = x;
+
+universal_func(x);
+universal_func(rx);
+universal_func(cx);
+universal_func(std::move(x));
+universal_func(42);
+
+class my_class
+{
+public:
+
+    my_class(const my_class&);
+    my_class(my_class&&);
+
+    void method() &;
+    void method() &&;
+
+
+    
+};*/
+
+/*
+template <class... T>
+class mpl_vector;
+
+template <class TL1, class TL2>
+struct concatenate;
+
+template <template <class...> class TL1, template <class...> class TL2, class... L1, class... L2>
+struct concatenate<TL1<L1...>, TL2<L2...>>
+{
+};
+
+concatenate<mpl_vector<int, double, float>, mpl_vector<string, char>>;*/
 #endif
